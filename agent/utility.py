1d694200b4bc09d4a4f2f19971b60508d729c33d
mcarmanize <20862667+mcarmanize@users.noreply.github.com>
2022-10-17 22:31:25 -0600

    tuned goodlist check to avoid less informational events

--- agent/utility.py
+++ agent/utility.py
@@ -109,8 +109,9 @@ def get_event_string(event):
                     event["responsible_pid_command"],
                     event["ppid_command"],
                 )
-            elif "command" in event_keys:
-                event_string = "{},{}".format(event["event"], event["command"])
+            # tuning down the filtering for process events that have limited information
+            # elif "command" in event_keys:
+            #     event_string = "{},{}".format(event["event"], event["command"])
         elif "file" in event_keys:
             if "original" in event_keys:
                 event_string = "{},{},{},{}".format(
