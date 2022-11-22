f8a450b80e0ee21f4fba17178aea7a28277ac570
mcarmanize <20862667+mcarmanize@users.noreply.github.com>
2022-11-22 07:15:29 -0700

    minimal processing of eslogger events before adding to database

--- agent/eslogger.py
+++ agent/eslogger.py
@@ -2,7 +2,21 @@
 """
     wrapping eslogger for macOS Ventura and greater
 
-    it looks like we'll need to redo filtering for the new event format
+    esfriend - a minimal malware analysis sandbox framework for macOS
+    Copyright (C) <2022> Matt Carman
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
 
 import sys
@@ -13,6 +27,7 @@ import hashlib
 from database import DatabaseConnection
 from bson.objectid import ObjectId
 from agent_config import MONGO_CONNECTION_STRING, ESLOGGER
+from utility import get_process_data
 
 
 ESLOGGER_PID = os.getpid()
@@ -56,7 +71,11 @@ class ESLogger(object):
 
     def insert_event(self, line):
         event = json.loads(line)
-        self.db.run_logs[self.job_id+"eslog"].insert_one(event)
+        event["pid"] = event["process"]["audit_token"]["pid"]
+        event["process_path"] = event["process"]["executable"]["path"]
+        event["event_type_description"] = list(event["event"].keys())[0]
+        event = get_process_data(event)
+        self.db.eslog.insert_one(event)
 
 
 if __name__ == "__main__":
