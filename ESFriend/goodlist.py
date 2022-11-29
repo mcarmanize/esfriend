0075d059cd07d6431d49a595dd3357cf8134b548
mcarmanize <20862667+mcarmanize@users.noreply.github.com>
2022-11-29 08:13:04 -0700

    not complete: doing goodlist server side

--- ESFriend/goodlist.py
+++ ESFriend/goodlist.py
@@ -33,6 +33,7 @@ class DatabaseConnection:
     def __init__(self, connection_string):
         try:
             self.client = MongoClient(connection_string)
+            self.jobs = self.client.esfriend["jobs"]
             self.run_logs = self.client.run_logs
         except Exception as err:
             print(err)
@@ -41,54 +42,98 @@ class DatabaseConnection:
 class GoodList:
     def __init__(self, selection=None):
         self.db = DatabaseConnection("mongodb://127.0.0.1:27017")
-        self.goodlist = self.db.client.esfriend["goodlist"]
-        self.goodlist.create_index("event_md5")
+        # endpoint security goodlist
+        self.esgoodlist = self.db.client.esfriend["esgoodlist"]
+        # log stream goodlist
+        self.lsgoodlist = self.db.client.esfriend["lsgoodlist"]
+        self.esgoodlist.create_index("event_md5")
+        self.lsgoodlist.create_index("event_md5")
         self.select_run_log()
 
     def select_run_log(self):
-        self.collections = self.db.run_logs.list_collections()
-        self.collections = sorted(self.collections, key=lambda d: d["name"])
+        # self.collections = self.db.run_logs.list_collections()
+        # self.collections = sorted(self.collections, key=lambda d: d["name"])
+        jobs = self.db.jobs.find()
         os.system("clear")
-        collection_index = 0
-        collection_name_list = []
-        print("\n   Select run to add to goodlist database:\n")
+        job_index = 0
+        job_list = []
+        print("\n   Select job to add to goodlist database:\n")
 
-        for collection in self.collections:
-            collection_name = collection["name"]
-            if not str(collection_name).endswith("syslog") and not str(collection_name).endswith("eslog"):
-                job_data = self.db.client.esfriend.jobs.find_one(
-                    {"_id": ObjectId(collection_name)}
-                )
-                file_name = job_data["file_name"]
-                timeout = job_data["timeout"]
-                if "tags" in job_data.keys():
-                    collection_tags = job_data["tags"]
-                else:
-                    collection_tags = None
-                collection_name_list.append(collection_name)
-                print(f"  {collection_index}: {file_name} {timeout} {collection_tags}")
-                collection_index += 1
+        for job in jobs:
+            if job["job_progress"] == 5:
+                file_name = job["file_name"]
+                timeout = job["timeout"]
+                tags = job["tags"]
+                job_list.append(job["_id"])
+                print(f"  {job_index}: {file_name} {timeout} {tags}")
+                job_index += 1
         selection = int(input("\n    Enter a number: "))
-        self.selected_collection = collection_name_list[selection]
-        self.process_run_log()
+        self.selected_job = job_list[selection]
+        self.process_run_logs()
 
-    def process_run_log(self):
-        cursor = self.db.run_logs[self.selected_collection].find(
-            {"event": {"$exists": True}}
-        )
-        for item in cursor:
-            if "event" in item.keys():
-                event_string = self.get_event_string(item)
-                event_md5 = hashlib.md5(event_string.encode("utf-8")).hexdigest()
-                event_ssdeep = ssdeep.hash(event_string.encode("utf-8"))
-                event_exists = self.goodlist.find_one({"event_md5": event_md5})
-                if event_exists is None:
-                    event_insert = {
-                        "event_string": event_string,
-                        "event_md5": event_md5,
-                        "event_ssdeep": event_ssdeep,
-                    }
-                    self.goodlist.insert_one(event_insert)
+    def process_run_logs(self):
+        es_collection_name = str(self.selected_job)+"eslog"
+        ls_collection_name = str(self.selected_job)+"syslog"
+        ls_cursor = self.db.run_logs[ls_collection_name].find()
+        for log_event in ls_cursor:
+            message_md5 = hashlib.md5(log_event["eventMessage"].encode("utf-8")).hexdigest()
+            message_ssdeep = ssdeep.hash(log_event["eventMessage"].encode("utf-8"))
+            event_exists = self.lsgoodlist.find_one({"message_md5": message_md5})
+            if event_exists is None:
+                event_insert = {
+                    "event_message": log_event["eventMessage"],
+                    "message_md5": message_md5,
+                    "message_ssdeep": message_ssdeep,
+                }
+                self.lsgoodlist.insert_one(event_insert)
+        es_cursor = self.db.run_logs[es_collection_name].find()
+        for event in es_cursor:
+            event_string = self.get_event_string(event)
+        
+        
+        # if "event" in item.keys():
+        #     event_string = self.get_event_string(item)
+        #     event_md5 = hashlib.md5(event_string.encode("utf-8")).hexdigest()
+        #     event_ssdeep = ssdeep.hash(event_string.encode("utf-8"))
+        #     event_exists = self.goodlist.find_one({"event_md5": event_md5})
+        #     if event_exists is None:
+        #         event_insert = {
+        #             "event_string": event_string,
+        #             "event_md5": event_md5,
+        #             "event_ssdeep": event_ssdeep,
+        #         }
+        #         self.goodlist.insert_one(event_insert)
+
+
+    def get_event_string2(self, event):
+        process_events = ["exec", "fork", "exit"]
+        file_events = ["create", "open", "write", "close", ]
+        try:
+            event_string = ""
+            event_type_description = event["event_type_description"]
+            process_path = event["process_path"]
+            pcommand = event["pcommand"]
+            rcommand = event["rcommand"]
+
+        except Exception as err:
+            print(f"{err}\n{event}\n")
+            return ""
+
+    def open_string(self, event):
+        event_string = "{},{},{}".format(event["event_type_description"], event["process_path"], event["event"]["open"]["file"]["path"])
+        return event_string
+
+    def close_string(self, event):
+        event_string = "{},{},{}".format(event["event_type_description"], event["process_path"], event["event"]["close"]["target"]["path"])
+        return event_string
+    
+    def create_string(self, event):
+        event_string = "{},{},{}".format(event["event_type_description"], event["process_path"], event["event"]["create"]["destination"]["existing_file"]["path"])
+        return event_string
+
+    def get_task_string(self, event):
+        event_string = "{},{},{}".format(event["event_type_description"], event["process_path"], event["event"]["open"]["file"]["path"])
+        return event_string
 
     def get_event_string(self, event):
         try:
